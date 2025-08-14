9d552cf9f68156b36092a2a2537cf4ad126360e4
Andrea Gutiérrez Alfaro <charecktowa@protonmail.com>
2025-08-13 21:07:31 -0600

    Created scraper for cic.ipn.mx pending db save

---
+++ scraper/scraper_saber.py
@@ -0,0 +1,10 @@
+# saber es otro sitio, por lo que tendrá sus propias funciones
+
+
+def saber_extraer_alumnos(url: str):
+    return None
+
+
+if __name__ == "__main__":
+    # TODO: Modificar para que no se pueda ejecutar directamente
+    main()
